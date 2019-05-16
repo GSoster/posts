8f26ca6093cf5fa23e13c80fe2383876fc60a278
Guilherme Soster Santos <Guilherme_Santos3@Dell.com>
2019-05-16 13:27:50 -0300

    added some modification to FilterCollection

--- YieldKeyword/YieldKeyword/FilterCollection.cs
+++ YieldKeyword/YieldKeyword/FilterCollection.cs
@@ -4,6 +4,28 @@ namespace YieldKeyword
 {
     public static class FilterCollection
     {
+        public static IEnumerable<int> CollectionOneToTen()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield return 6;
+            yield return 7;
+            yield return 8;
+            yield return 9;
+            yield return 10;
+        }
+
+        public static IEnumerable<int> CollectionOneToTenLoop()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                yield return i;
+            }
+        }
+
         public static IEnumerable<int> FilterNumbersGreaterThan(List<int> collection, int max)
         {
             for (int i = 0; i < collection.Count; i++)
